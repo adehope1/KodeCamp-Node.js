b47e5f672bac666b088f1faa3f73b14e81e44fea
adehope1 <adeoyeademola@gmail.com>
2024-06-20 00:29:46 +0100

    update

--- Task2/formatName.js
+++ Task2/formatName.js
@@ -1,7 +1,15 @@
-function formatName(names) {
-         
-    return names.trim(" ").replace("   ", "").toUpperCase("" , " " )
+// A function that trim spaces and also change name of the first initials that are small case to upper case
+function formatName(str) {
+    // Trim the words to remove space from it
+    const trimmedStr = str.trim("");
 
-};
-
-console.log(formatName("  victor    red"));
+//   create a variable and spilt the words for easily capitalization
+    const words = trimmedStr.split('  ');
+        // Capitalize the first letter of each word
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  
+    // Join the capitalized words back into a string
+    return capitalizedWords.join(' ');
+  }
+  console.log(formatName (" victor  red "));
+  
--- Task2/formatThousand.js
+++ Task2/formatThousand.js
@@ -1,8 +1,24 @@
-function formatThousand(num) {
-    let digits =window.prompt("your digits here");
-     digits = parseInt(digits);
-    let Outputs = digits.toLocalString("");
-    return num;
-    
-}
-console.log(formatThousand(10000000));
+// a function that will add a sepertor after every 3 digit that are not decimal
+function formatThousand(number) {
+    // Convert the digits to a string
+    const numString = number.toString();
+  
+    // Split the string into integer
+    const [integerPart = ""] = numString.split(".");
+  
+    // Reverse the integer part for easier processing
+    const reversedInteger = integerPart.split("").reverse().join("");
+  
+    // Add commas every 3 digits using slice and join
+    const formattedInteger = reversedInteger.replace(/(?!^)(...)/g, "$1,");
+  
+    // Reverse the formatted integer back to normal order
+    const finalInteger = formattedInteger.split("").reverse().join("");
+  
+    // Combine formatted integer and decimal part (if any)
+    return finalInteger ;
+  }
+  
+  console.log(formatThousand(10000000)); 
+  
+  
